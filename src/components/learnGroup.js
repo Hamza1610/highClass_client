4726238e5895a129c367ab391b1748041cb468d1
Hamza1060 <muhammadhamza162003@gmail.com>
2023-10-09 21:31:46 +0100

    added room change feature

--- src/components/learnGroup.js
+++ src/components/learnGroup.js
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import './learnGroup.css';
-
+import Room from './room';
 const LearnGroup = () => {
+    const [roomOne, setRoomOne] = useState(true);
+    const [roomTwo, setRoomTwo] = useState(false);
+
+    const roomOneSetter = () => {
+        setRoomOne(true);
+        setRoomTwo(false);
+    }
+    const roomTwoSetter = () => {
+        setRoomTwo(true);
+        setRoomOne(false);
+    }
     return (
         <div className="learn-group">
-            
+            <div className='learn-group-nav' >
+                <div className='rooms-div'>
+                    <button onClick={roomOneSetter}>Room 1</button>
+                    <button onClick={roomTwoSetter}>Room 2</button>
+                </div>
+                <div className='search-div'>
+                    <input className="search-input" type="search" />
+                    <button className='search-btn'>Search</button>
+                </div>
+            </div>
+            {/* To get chats for both Room two collections are created and useEffect is used to fetch them */}
+            {roomOne && (<Room/>)}
+            {roomTwo && (<Room/>)}
         </div>
     )
 }
