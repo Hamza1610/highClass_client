90ac4e51f7dd5abd4a27e10f4f7db776774b9a61
Hamza1060 <muhammadhamza162003@gmail.com>
2023-10-16 06:24:53 +0100

    change mapped to 4 var

--- src/components/editInput.js
+++ src/components/editInput.js
@@ -1,16 +1,80 @@
-
+import './editInput.css'
 import { useState } from 'react';
 const EditInput = (props) => {
-    const [fromEdit, setFromEdit] = useState('');
-    const [toEdit, setToEdit] = useState('');
-    const handleChange = () => {
-        console.log(fromEdit, toEdit);
+
+    const [fromOne, setFromOne] = useState('');
+    const [toOne, setToOne] = useState('');
+
+    const [fromTwo, setFromTwo] = useState('');
+    const [toTwo, setToTwo] = useState('');
+
+    const [fromThree, setFromThree] = useState('');
+    const [toThree, setToThree] = useState('');
+
+    const [fromFour, setFromFour] = useState('');
+    const [toFour, setToFour] = useState('');
+
+    const handleEdit = async () => {
+        const day = localStorage.getItem('editDay');
+        const reg_num = localStorage.getItem('regNumber');
+        const editData = [];
+        editData.push({ from: fromOne, to: toOne })
+        editData.push({ from: fromTwo, to: toTwo})
+        editData.push({ from: fromThree, to: toThree })
+        editData.push({ from: fromFour, to: toFour })
+
+        const data = { reg_num: reg_num, editedData: editData , day: day}
+        console.log(data)
+
+        const response = await fetch('/edit-time', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+              },
+        })
+
+        const json = response.json();
+        if (!response.ok) {
+            console.log(json.error);
+        }
+        
+        if (response && response.ok) {
+            console.log(response);
+            props.saveEdit()
+            console.log('Data edited successfully');
+        }
+    // console.log(data)
     }
-    handleChange();
+
     return (
         <div className="time-edit">
-            <input type="text" defaultValue={props.time.from} onChange={(e)=> setFromEdit(e.target.value)}/>
-            <input type="text" defaultValue={props.time.to}  onChange={(e)=> setToEdit(e.target.value)}/>
+            <div>Edit {} schedule</div>
+            <div className='edit-input-pair'>
+                <input id='from' name='from' type="time" value={fromOne} onChange={(e)=> setFromOne(e.target.value)}/>
+                <span>to:  </span>
+                <input id='to' name='to' type="time" value={toOne}  onChange={(e)=> setToOne(e.target.value)}/>
+            </div>
+
+            <div className="edit-input-pair">
+                <input id='from' name='from' type="time" value={fromTwo}  onChange={(e)=> setFromTwo(e.target.value)}/>
+                <span>to:  </span>
+                <input type="time" value={toTwo}  onChange={(e)=> setToTwo(e.target.value)}/>
+            </div>
+
+            <div className="edit-input-pair">
+                <input id='from' name='from' type="time" value={fromThree}  onChange={(e)=> setFromThree(e.target.value)}/>
+                <span>to:  </span>
+                <input id='to' name='to' type="time" value={toThree}  onChange={(e)=> setToThree(e.target.value)}/>
+            </div>
+            
+            <div className="edit-input-pair">
+                <input id='from' name='from' type="time" value={fromFour}  onChange={(e)=> setFromFour(e.target.value)}/>
+                <span>to:  </span>
+                <input id='to' name='to' type="time" value={toFour}  onChange={(e)=> setToFour(e.target.value)}/>
+            </div>
+        
+            <button className='save-edit' onClick={handleEdit}>Save</button>
         </div>
     )
 }
